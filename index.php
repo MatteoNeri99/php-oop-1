fdb77130a570babbb21b2069c4dee4e7af71460e
MatteoNeri99 <matteolavoro99@gmail.com>
2024-07-03 07:20:28 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+
+class Movie{
+
+    public $name;
+    public $year;
+    public $rating;
+
+    public function __construct($name, $year, $rating){
+        $this->name=$name;
+        $this->year=$year;
+        $this->name=$rating;
+
+    }
+
+}
